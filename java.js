af248eeb8877e9c28b0d11cf8d53038c1e7aba4e
nwansley <44908595+nwansley@users.noreply.github.com>
2020-05-14 11:23:49 -0700

    Update java.js

--- java.js
+++ java.js
@@ -36,47 +36,47 @@ function enlargeImg3() {
 function enlargeImg4() {
 	modal.style.display = "block";
 	modalImg.src = "images/jacket.jpg";
-	music.pause();
+	playMusic()
 }
 function enlargeImg5() {
 	modal.style.display = "block";
 	modalImg.src = "images/tree.jpg";
-	music.pause();
+	playMusic()
 }
 function enlargeImg6() {
 	modal.style.display = "block";
 	modalImg.src = "images/water.jpeg";
-	music.pause();
+	playMusic()
 }
 function enlargeImg7() {
 	modal.style.display = "block";
 	modalImg.src = "images/melrose.jpg";
-	music.pause();
+	playMusic()
 }
 function enlargeImg8() {
 	modal.style.display = "block";
 	modalImg.src = "images/sun.jpg";
-	music.pause();
+	playMusic()
 }
 function enlargeImg9() {
 	modal.style.display = "block";
 	modalImg.src = "images/think.jpg";
-	music.pause();
+	playMusic()
 }
 function enlargeImg10() {
 	modal.style.display = "block";
 	modalImg.src = "images/yearbook.jpg";
-	music.pause();
+	playMusic()
 }
 function enlargeImg11() {
 	modal.style.display = "block";
 	modalImg.src = "images/glass.jpg";
-	music.pause();
+	playMusic()
 }
 function enlargeImg12() {
 	modal.style.display = "block";
 	modalImg.src = "images/beca.jpg";
-	music.pause();
+	playMusic()
 }
 function enlargeImg13() {
 	modal.style.display = "block";
@@ -86,32 +86,32 @@ function enlargeImg13() {
 function enlargeImg14() {
 	modal.style.display = "block";
 	modalImg.src = "images/dark.JPG";
-	music.pause();
+	playMusic()
 }
 function enlargeImg15() {
 	modal.style.display = "block";
 	modalImg.src = "images/boba.jpg";
-	music.pause();
+	playMusic()
 }
 function enlargeImg16() {
 	modal.style.display = "block";
 	modalImg.src = "images/beca2.jpg";
-	music.pause();
+	playMusic()
 }
 function enlargeImg17() {
 	modal.style.display = "block";
 	modalImg.src = "images/devin.jpg";
-	music.pause();
+	playMusic()
 }
 function enlargeImg18() {
 	modal.style.display = "block";
 	modalImg.src = "images/down.jpg";
-	music.pause();
+	playMusic()
 }
 
 function closeModal() {
 	modal.style.display = "none";
-	music.play();
+	playMusic()
 }
 
 
